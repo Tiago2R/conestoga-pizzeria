b59dd1ea442d4f740a90baa398e0e66ca995106a
Tiago Ribeiro <T2R@Tiagos-MacBook-Air.local>
2015-10-04 23:26:13 -0400

    Added logo and basic form

--- index.php
+++ index.php
@@ -1,9 +1,66 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	</head>
 	<body>
-		<p>Conestoga Pizzeria</p>
+		<div class="container">
+			<div class="logo">
+				<img src="imgs/logo.png" alt="Conestoga Pizzeria logo">
+			</div>
+		</div>
+		<div>
+			<form>
+				<div class="row">
+					<div class="col-sm-6">
+						<p>Personal Details</p>
+						<div class="form-group">
+							<label for="inputName">Name</label>
+							<input type="text" class="form-control" id="inputName" placeholder="Name">
+						</div>
+						<div class="form-group">
+							<label for="inputAddress">Address</label>
+							<input type="text" class="form-control" id="inputAddress" placeholder="Address">
+						</div>
+						<div class="form-group">
+							<label for="inputAddress">Postal Code</label>
+							<input type="text" class="form-control" id="inputPostalCode" placeholder="Postal Code">
+						</div>
+						<div class="form-group">
+							<label for="inputAddress">City</label>
+							<input type="text" class="form-control" id="inputCity" placeholder="City">
+						</div>
+						<div class="form-group">
+							<label for="inputProvince">Province</label>
+							<input type="text" class="form-control" id="inputProvince" placeholder="Province">
+						</div>
+						<div class="form-group">
+							<label for="inputTelephone">Telephone</label>
+							<input type="text" class="form-control" id="inputTelephone" placeholder="Telephone">
+						</div>
+						<div class="form-group">
+							<label for="inputEmailAddress">Email Address</label>
+							<input type="text" class="form-control" id="inputEmailAddress" placeholder="Email Address">
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<p>Order Information</p>
+						<div class="form-group">
+							<label for="inputToppings">Size</label>
+							<input type="text" class="form-control" id="inputSize" placeholder="Size">
+						</div>
+						<div class="form-group">
+							<label for="inputToppings">Crust Type</label>
+							<input type="text" class="form-control" id="inputToppings" placeholder="Crust Type">
+						</div>
+						<div class="form-group">
+							<label for="inputToppings">Toppings</label>
+							<input type="text" class="form-control" id="inputToppings" placeholder="Toppings">
+						</div>
+					</div>
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+		</div>		
 	</body>
 </html>
